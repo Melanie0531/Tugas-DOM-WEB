7cb7e019dcd1d1b2ec2ddf3ea92fdf8584548376
Melanie0531 <melanieputu10@gmail.com>
2024-08-21 14:03:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementById('tanda1').addEventListener('click', function() {
+
+    var table = document.getElementById('melku');
+
+
+    if (table.style.display === 'none') {
+        table.style.display = 'block';
+    } else {
+        table.style.display = 'none';
+    }
+});
+
+document.getElementById('tanda2').addEventListener('click', function() {
+
+    var table = document.getElementById('gloriaku');
+
+
+    if (table.style.display === 'none') {
+        table.style.display = 'block';
+    } else {
+        table.style.display = 'none';
+    }
+});
+
+document.getElementById('tanda3').addEventListener('click', function() {
+
+    var table = document.getElementById('mangsriku');
+
+
+    if (table.style.display === 'none') {
+        table.style.display = 'block';
+    } else {
+        table.style.display = 'none';
+    }
+});
